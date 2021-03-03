84c685e6f9819ec995b4c6d51b8270129479d578
The-Pascal <chaharchandresh@gmail.com>
2021-03-03 16:55:48 +0530

    Added home activity

--- app/src/main/java/com/example/yourrecycler/Home.kt
+++ app/src/main/java/com/example/yourrecycler/Home.kt
@@ -1,51 +1,63 @@
 package com.example.yourrecycler
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.yourrecycler.registerLogin.RegistrationPage
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_home.*
 
+
 class Home : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setSupportActionBar(toolbar)
+        val adapter = GroupAdapter<ViewHolder>()
+        home_recyler_view.adapter = adapter
+
+
+        adapter.add(AddRecycleItem());
+        adapter.add(AddRecycleItem());
+        adapter.add(AddRecycleItem());
+        adapter.add(AddRecycleItem());
+
+    }
+}
+
+class AddRecycleItem(): Item<ViewHolder>(){
+    override fun getLayout(): Int {
+        return R.layout.user_items_recycle
     }
 
+    override fun bind(viewHolder: ViewHolder, position: Int) {
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.new_messages_menu ->{
-//                startActivity(Intent(this, newmessageActivity::class.java))
-            }
-            R.id.profile -> {
-//                startActivity(Intent(this, Profile::class.java))
-            }
-            R.id.sign_out_menu ->{
-
-                val uid = FirebaseAuth.getInstance().uid
-                FirebaseDatabase.getInstance().getReference("/Users/$uid/active")
-                    .setValue(false)
-
-                FirebaseAuth.getInstance().signOut()
-                val intent = Intent(this, RegistrationPage::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or ( Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
 }
+
+//class ChatFromItem(): Item<ViewHolder>() {
+//
+//    override fun bind(viewHolder: ViewHolder, position: Int) {
+////            viewHolder.itemView.textView_from_row.text = text
+////            Picasso.get().load(image).into(viewHolder.itemView.imageView_from_row)
+//    }
+//    override fun getLayout(): Int {
+//        return R.layout.user_items_recycle
+//    }
+//
+//}
